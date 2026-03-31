3cbfde604f80ee5ee9351c290bf684487b6435ad
RealMG <realmg51@gmail.com>
2026-03-31 17:10:54 +0700

    Khởi đầu của chính mình!

---
+++ src/main.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Myself
+{
+  class orogram
+  {
+    static void Main()
+    {
+      StartLogging()
+    }
+    static void StartLogging()
+    {
+      Console.WriteLine("Myself v0.0.1-demo start Sucessfull!");
+    }
+  }
+}
